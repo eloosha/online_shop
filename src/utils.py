3977d24205f4e15540d5d13ab443793797b9b583
Ilya Tokmyanin <itokm@inbox.ru>
2026-03-15 18:33:09 +0200

    sdelal dz

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import json
+import os
+from src.main import Category
+from src.main import Product
+
+
+def read_json(path: str) -> dict:
+    full_path = os.path.abspath(path)
+    with open(full_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+
+def create_objects_from_json(data):
+    categories = []
+
+    for category in data:
+
+        products = []
+
+        for product in category['products']:
+            product_obj = Product(**product)
+            products.append(product_obj)
+
+        category_obj = Category(
+            name=category['name'],
+            description=category['description'],
+            products=products
+        )
+
+        categories.append(category_obj)
+
+    return categories
---
+++ tests/test_main.py
@@ -0,0 +1,46 @@
+import pytest
+from src.main import Category
+from src.main import Product
+
+
+def test_product_init(my_product):
+    assert my_product[0].name == 'Арбуз'
+    assert my_product[1].description == 'оранжевый'
+    assert my_product[2].price == 55
+    assert my_product[3].quantity == 5
+
+
+def test_category_init(my_category):
+    assert my_category.name == 'Фрукты'
+    assert len(my_category.products) == 4
+    assert my_category.products[0].name == 'Арбуз'
+
+
+def test_counter_one(my_category):
+    assert Category.category_count == 1
+    assert Category.products_count == 4
+
+
+def test_counter_multiple():
+    product_apple = Product('Яблоко', 'красное', 45, 5)
+    category_fruits = Category('Фрукты', 'обычные', [product_apple])
+
+    assert Category.category_count == 1
+    assert Category.products_count == 1
+
+    product_tomato = Product('Помидор', 'красный', 35, 8)
+    category_fruits = Category('Овощи', 'стандартные', [product_tomato])
+
+    assert Category.category_count == 2
+    assert Category.products_count == 2
+
+
+def test_counter_with_multiple_products():
+    product_apple = Product('Яблоко', 'красное', 45, 5)
+    product_banana = Product('Банан', 'желтый', 65, 2)
+    product_orange = Product('Апельсин', 'оранжевый', 55, 6)
+
+    category = Category('Фрукты', 'спелые', [product_apple, product_banana, product_orange])
+
+    assert Category.category_count == 1
+    assert Category.products_count == 3
