1aa86697daf32b9b46db1300ed6b0bfa4b5b9d1e
Ilya Tokmyanin <itokm@inbox.ru>
2026-04-20 17:46:29 +0200

    na proverku

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,8 +10,8 @@ def test_product_init(my_product):
 
 def test_category_init(my_category):
     assert my_category.name == "Фрукты"
-    assert len(my_category.products) == 4
-    assert my_category.products[0].name == "Арбуз"
+    assert len(my_category._products) == 4
+    assert my_category._products[0].name == "Арбуз"
 
 
 def test_counter_one(my_category):
@@ -44,3 +44,56 @@ def test_counter_with_multiple_products():
 
     assert Category.category_count == 1
     assert Category.products_count == 3
+
+
+def test_price_setter():
+    my_product = Product("Яблоко", "красное", 45, 5)
+
+    my_product.price = -1
+    assert my_product.price == 45
+
+    my_product.price = 0
+    assert my_product.price == 45
+
+    my_product.price = 50
+    assert my_product.price == 50
+
+
+def test_new_product():
+    product_dict = {
+        "name": "55 QLED 4K",
+        "description": "Фоновая подсветка",
+        "price": 123000.0,
+        "quantity": 7,
+    }
+
+    my_new_product = Product.new_product(product_dict)
+    assert my_new_product.name == "55 QLED 4K"
+    assert my_new_product.description == "Фоновая подсветка"
+    assert my_new_product.price == 123000.0
+    assert my_new_product.quantity == 7
+
+
+def test_products_getter(my_category):
+    result = my_category.products
+
+    assert result[0] == "Арбуз, 300 руб. Остаток: 10"
+    assert result[1] == "Мандарин, 25 руб. Остаток: 25"
+
+
+def test_add_product(my_category):
+    new_product1 = Product("Арбуз", "зелено-красный", 500, 3)
+    my_category.add_product(new_product1)
+
+    assert my_category._products[0].price == 500
+    assert my_category._products[0].quantity == 13
+    assert len(my_category._products) == 4
+
+    new_product2 = Product("Киви", "зеленый", 400, 10)
+    my_category.add_product(new_product2)
+
+    assert my_category._products[4].name == "Киви"
+    assert my_category._products[4].description == "зеленый"
+    assert my_category._products[4].price == 400
+    assert my_category._products[4].quantity == 10
+    assert len(my_category._products) == 5
